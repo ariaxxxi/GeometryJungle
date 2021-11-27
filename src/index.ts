de275a8e42c0cdb09bf23eba5782453a3acf2c01
xxxi <87568028+ariaxxxi@users.noreply.github.com>
2021-11-27 02:13:49 -0500

    enter

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
 import { sphere_with_waves } from "./sphere_with_waves";
 import { terrain_editor } from "./terrain_editor";
-import * as dat from 'dat.gui'
-
-// const gui = new dat.GUI();
-// var state = 0;
-// // BUTTON
-// function SphereButton() {
-//     var fooSphere = sphere_with_waves();
-//     var fooTerrain = terrain_editor();
-//     // fooSphere.GUI();
-//     // fooSphere.animate();
-// }
-
-// function TerrainButton() {
-//     terrain_editor();
-// }
-
-// var sphere = {
-//     Sphere_with_waves: SphereButton
-// };
-
-
-// var terrain = {
-//     Terrain_editor: TerrainButton
-// };
-
-
-// gui.add(sphere, 'Sphere_with_waves');
-// gui.add(terrain, 'Terrain_editor');
+// import * as dat from 'dat.gui'
 
 
 // sphere_with_waves();
